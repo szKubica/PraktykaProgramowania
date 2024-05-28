9757c4cc889652cbf87c7aa920f62e69023213ba
Szymon Kubica <102176368+szKubica@users.noreply.github.com>
2024-05-28 18:13:39 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,26 +5,24 @@ This module contains the utility functions for the calculator app.
 
 def add(a: int, b: int) -> int:
     """
-    This function adds two numbers and returns the result.
+    Dodawanie
     """
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
     """
-    This function subtracts two numbers and returns the result."""
+    Odejmowanie"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
     """
-    This function multiplies two numbers and returns the result.
-    """
+    Mnozenie"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
     """
-    This function divides two numbers and returns the result.
-    """
+    Dzielenie"""
     return a / b
